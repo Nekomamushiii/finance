a6548f308af0f7849e812e96b1e3a4c973a276b4
Nekomamushi <hurlee.huu132@gmail.comgit config --global user.email hurlee.huu132@gmail.com>
2020-06-01 11:44:39 +0900

    hicheel

--- app.js
+++ app.js
@@ -1,14 +1,86 @@
-var uiController = (function () {})();
-
-var financeController = (function () {})();
-
+var uiController = (function () {
+  var DOMstring = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstring.inputType).value,
+        description: document.querySelector(DOMstring.inputDescription).value,
+        value: document.querySelector(DOMstring.inputValue).value,
+      };
+    },
+    getDOMstring: function () {
+      return DOMstring;
+    },
+  };
+})();
+var financeController = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function () {
+      return data;
+    },
+  };
+})();
 var appController = (function (m1, m2) {
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    console.log("ажилж байна");
-  });
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13) {
-      console.log("enter darlaa");
-    } else console.log("өөр товч дарсан байна: " + event.keyCode);
-  });
+  var ctrlAddItem = function () {
+    var input = m1.getInput();
+    m2.addItem(input.type, input.description, input.value);
+    console.log(m1.getInput());
+    console.log("ajillaj ehellee");
+  };
+  var setUpEventListeners = function () {
+    var DOM = m1.getDOMstring();
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
+      ctrlAddItem();
+    });
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  return {
+    init: function () {
+      console.log("app started");
+      return setUpEventListeners();
+    },
+  };
 })(uiController, financeController);
+appController.init();
