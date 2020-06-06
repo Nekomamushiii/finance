46fc0e3d53a1b340df277ef73b0e3111e6cf8173
Nekomamushi <hurlee.huu132@gmail.comgit config --global user.email hurlee.huu132@gmail.com>
2020-06-06 12:23:39 +0900

    hicheel

--- app.js
+++ app.js
@@ -4,17 +4,46 @@ var uiController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
   };
   return {
     getInput: function () {
       return {
         type: document.querySelector(DOMstring.inputType).value,
         description: document.querySelector(DOMstring.inputDescription).value,
-        value: document.querySelector(DOMstring.inputValue).value,
+        value: parseInt(document.querySelector(DOMstring.inputValue).value),
       };
     },
     getDOMstring: function () {
       return DOMstring;
+    },
+    clearFields: function () {
+      var fields = document.querySelectorAll(
+        DOMstring.inputDescription + ", " + DOMstring.inputValue
+      );
+      var fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach(function (el, index, array) {
+        array[0].value = "";
+        array[1].value = 0;
+      });
+      fieldsArr[0].focus();
+    },
+    addListItem: function (item, type) {
+      var html, list;
+      if (type === "inc") {
+        list = DOMstring.incomeList;
+        html =
+          '<div class="item clearfix" id="income-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else {
+        list = DOMstring.expenseList;
+        html =
+          '<div class="item clearfix" id="expense-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      html = html.replace("$$ID", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
     },
   };
 })();
@@ -29,6 +58,13 @@ var financeController = (function () {
     this.description = description;
     this.value = value;
   };
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach(function (el) {
+      sum = sum + el.value;
+    });
+    data.totals[type] = sum;
+  };
   var data = {
     items: {
       inc: [],
@@ -38,8 +74,24 @@ var financeController = (function () {
       inc: 0,
       exp: 0,
     },
+    tusuv: 0,
+    huvi: 0,
   };
   return {
+    tusuvTootsooloh: function () {
+      calculateTotal("inc");
+      calculateTotal("exp");
+      data.tusuv = data.totals.inc - data.totals.exp;
+      data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+    tusviigAvah: function () {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+      };
+    },
     addItem: function (type, desc, val) {
       var item, id;
       if (data.items[type].length === 0) id = 1;
@@ -52,7 +104,9 @@ var financeController = (function () {
         item = new Expense(id, desc, val);
       }
       data.items[type].push(item);
+      return item;
     },
+
     seeData: function () {
       return data;
     },
@@ -61,9 +115,15 @@ var financeController = (function () {
 var appController = (function (m1, m2) {
   var ctrlAddItem = function () {
     var input = m1.getInput();
-    m2.addItem(input.type, input.description, input.value);
-    console.log(m1.getInput());
-    console.log("ajillaj ehellee");
+
+    if (input.description !== "" && input.value !== 0) {
+      var item = m2.addItem(input.type, input.description, input.value);
+      m1.addListItem(item, input.type);
+      m1.clearFields();
+      m2.tusuvTootsooloh();
+      var tusuv = m2.tusviigAvah();
+      console.log(tusuv);
+    }
   };
   var setUpEventListeners = function () {
     var DOM = m1.getDOMstring();
