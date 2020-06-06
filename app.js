996046d05c37f19ad2b5554e2bf6264a2a6ed055
Nekomamushi <hurlee.huu132@gmail.comgit config --global user.email hurlee.huu132@gmail.com>
2020-06-06 18:37:02 +0900

    hicheel

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var uiController = (function () {
     addBtn: ".add__btn",
     incomeList: ".income__list",
     expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
+    containerDiv: ".container",
   };
   return {
     getInput: function () {
@@ -29,16 +34,34 @@ var uiController = (function () {
       });
       fieldsArr[0].focus();
     },
+    tusviigUzuuleh: function (tusuv) {
+      document.querySelector(DOMstring.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstring.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstring.expenseLabel).textContent =
+        tusuv.totalExp;
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstring.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstring.percentageLabel).textContent =
+          tusuv.huvi;
+      }
+    },
+    deleteListItem: function (id) {
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
     addListItem: function (item, type) {
       var html, list;
       if (type === "inc") {
         list = DOMstring.incomeList;
         html =
-          '<div class="item clearfix" id="income-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="inc-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else {
         list = DOMstring.expenseList;
         html =
-          '<div class="item clearfix" id="expense-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+          '<div class="item clearfix" id="exp-$$ID$$"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
       html = html.replace("$$ID", item.id);
       html = html.replace("$$DESCRIPTION$$", item.description);
@@ -92,6 +115,15 @@ var financeController = (function () {
         totalExp: data.totals.exp,
       };
     },
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map(function (el) {
+        return el.id;
+      });
+      var index = ids.indexOf(id);
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
+    },
     addItem: function (type, desc, val) {
       var item, id;
       if (data.items[type].length === 0) id = 1;
@@ -120,10 +152,14 @@ var appController = (function (m1, m2) {
       var item = m2.addItem(input.type, input.description, input.value);
       m1.addListItem(item, input.type);
       m1.clearFields();
-      m2.tusuvTootsooloh();
-      var tusuv = m2.tusviigAvah();
-      console.log(tusuv);
+      updateTusuv();
     }
+  };
+  var updateTusuv = function () {
+    m2.tusuvTootsooloh();
+    var tusuv = m2.tusviigAvah();
+    m1.tusviigUzuuleh(tusuv);
+    console.log(tusuv);
   };
   var setUpEventListeners = function () {
     var DOM = m1.getDOMstring();
@@ -135,10 +171,30 @@ var appController = (function (m1, m2) {
         ctrlAddItem();
       }
     });
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function (event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+          console.log(type + "====> " + itemId);
+          m2.deleteItem(type, itemId);
+          m1.deleteListItem(id);
+          updateTusuv();
+        }
+      });
   };
   return {
     init: function () {
       console.log("app started");
+      m1.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
       return setUpEventListeners();
     },
   };
